66911d966e439887ae1af4565fc2c5eab3ac1f4d
AstoRut <viberate@aol.com>
2023-02-23 16:56:09 -0500

    modal homework

--- app/js/index.js
+++ app/js/index.js
@@ -27,11 +27,31 @@ el.innerHTML = recipeName;
 
 // ==
 
+var data = {
+  pick_it: "This is info about how to pick a recipe.",
+
+  cook_it: "This is info about how to cook a recipe.",
+
+  store_it: "This is info about how to store a recipe.",
+};
+
 var modal = document.querySelector(".modal");
 var modalOuter = document.querySelector(".modal-outer");
 
+var modalh3 = document.querySelector(".modal h3");
+var modalinfo = document.querySelector(".modal p");
+
 function showPopover(event) {
-  if (!event.target.matches(".beta, .closer")) return;
+  //if (!event.target.matches(".beta, .closer")) return;
+  if (!event.target.matches(".beta, .closer, li a")) return;
+  if (event.target.matches("li a")) {
+    let name = event.target.innerHTML.replace(" ", "_");
+    modalh3.innerText = event.target.innerHTML;
+    modalinfo.innerText = data[name];
+  } else {
+    modalh3.innerText = "Hi! I'm a Modal Window (ʘ‿ʘ)╯";
+    modalinfo.innerText = "Information about the beta program.";
+  }
   modalOuter.classList.toggle("open");
   event.preventDefault();
 }
